9538f256c646e1587bbeb2dd345eec9008c89afb
jes009 <jes009@alumni.com>
2020-04-29 20:31:35 +0200

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,6 +1,6 @@
-       function Search(argument){
+       function SearchSp(argument){
 //cogemos los elementos con el id tabla y los guardamos en la constante
-        const tabla = document.getElementById('tabla');
+        const tabla = document.getElementById('tablasp');
 //pillamos el valor introducido por el usuario y lo pasamos a minus
         const valorbusqueda = document.getElementById('miBusqueda').value.toLowerCase();
 
@@ -52,6 +52,67 @@
 
 
         }
+
+
+       function SearchVl(argument){
+//cogemos los elementos con el id tabla y los guardamos en la constante
+        const tabla = document.getElementById('tablavl');
+//pillamos el valor introducido por el usuario y lo pasamos a minus
+        const valorbusqueda = document.getElementById('miBusqueda').value.toLowerCase();
+
+            let total = 0;
+
+            // pasamos por todas las filas y nos llevamos el contenido
+
+            for (let i = 1; i < tabla.rows.length; i++) {
+
+                let found = false;
+
+                const celdas = tabla.rows[i].getElementsByTagName('td');
+
+                // pasamos por todas las celdas
+
+                for (let i = 0; i < celdas.length && !found; i++) {
+
+                    const compareWith = celdas[i].innerHTML.toLowerCase();
+
+                    // comparamos el valor del usuario con el contenido de la celda
+
+                    if (valorbusqueda.length == 0 || compareWith.indexOf(valorbusqueda) > -1) {
+
+                        found = true;
+
+                        total++;
+
+                    }
+
+                }
+
+                if (found) {
+
+                    tabla.rows[i].setAttribute('class', 'mostra');;
+
+                } else {
+
+                    // si no ha encontrado ninguna coincidencia, esconde la
+
+                    // fila de la tabla
+
+                    tabla.rows[i].setAttribute('class', 'oculta');;
+
+                }
+
+            }
+
+ 
+
+
+        }
+
+
+
+
+
 
        function idioma(argument){
 //Pilla el elemento de seleccion
